59ecdf224cc40b8c1505d51a9493fc8131b56bb3
liuxin <liuxinchn@foxmail.com>
2022-06-13 10:29:35 +0800

    remove unnecessary files

--- 2Cmake/build/CMakeFiles/test.dir/src/world.obj.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/test.dir/src/world.obj: E:\CMakeTutorials\2Cmake\src\world.cpp \
- e:\mingw\include\stdio.h e:\mingw\include\_mingw.h \
- e:\mingw\include\msvcrtver.h e:\mingw\include\w32api.h \
- e:\mingw\include\sdkddkver.h e:\mingw\include\features.h \
- e:\mingw\lib\gcc\mingw32\9.2.0\include\stddef.h \
- e:\mingw\include\sys/types.h \
- e:\mingw\lib\gcc\mingw32\9.2.0\include\stdarg.h
